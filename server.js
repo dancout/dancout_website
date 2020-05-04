fde601834dd41fe6b7f679d2b55b10eb7e4bfa86
Daniel Couturier <dancout@umich.edu>
2020-05-03 22:58:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,7 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/dancout-website/index.html"));
 });
 
+app.use(express.static(path.join(__dirname, 'dist/dancout-website')));
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
