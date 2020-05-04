8a16bf3921f8c9704fe04a7d176351de43bc8c07
Daniel Couturier <dancout@umich.edu>
2020-05-03 22:25:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-
-
-app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname + '/dist/Wandri_Website/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/dancout-website/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
