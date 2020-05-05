5ded0db0b435387a1a51cc417c0858df7b6ab5ab
Daniel Couturier <dancout@umich.edu>
2020-05-04 21:44:08 -0400

    Initial Site setup (#2)
    * Rearrangements
    
    removed lots of the code I won't be using
    
    * Order Now Button
    
    Removed more dead code and added Order Now Button to scroll down
    
    * header sizing
    
    * Order Form mostly done
    
    Everything except for the above image is pretty much set up!
    
    * code cleanup
    
    radnom from fs
    
    * Default to first tab open
    
    * Updated Cover Photo
    
    * Moved Folder Structure
    
    Moved everything in `dancout-website` to the top level `dancout_website`.
    
    * Update package.json
    
    added angular cli and compiler-cli to dependencies
    
    * Update package.json
    
    added devkit build to dependencies
    
    * FIRE EVERYTHING
    
    Added all devDependencies to Dependencies
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Copied deploy from wandri
    
    * Update server.js
    
    * update package json
    
    * Update server.js
    
    * Update server.js

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTableModule,
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
